10833288de6a67856e6accf64178f1f893c9e7e1
TheAmid <45180999+TheAmid@users.noreply.github.com>
2020-11-16 19:31:02 +0400

    spm reslice on python test for spm migration

---
+++ opennft/reslice.py
@@ -0,0 +1,206 @@
+
+import numpy as np
+import sys
+
+class Python_Reslicing():
+
+    def __init__(self, parent=None):
+        super().__init__(parent=parent)
+
+    def spm_reslice(self, P, flags):
+
+        if flags.mask or flags.mean:
+            temp_x1 = np.transpose(np.array(range(1,P['dim'][0][0]+1), ndmin=2))
+            x1 = np.tile(temp_x1,(1,P['dim'][0][1]))
+            temp_x2 = np.array(range(1,P['dim'][0][1]+1), ndmin=2)
+            x2 = np.tile(temp_x2,(1,P['dim'][0][1]))
+
+            if flags.mean:
+                Count = np.zeros((int(P['dim'][0][0]),int(P['dim'][0][1]),int(P['dim'][0][2])))
+                Integral = np.zeros((int(P['dim'][0][0]),int(P['dim'][0][1]),int(P['dim'][0][2])))
+
+            if flags.mask:
+                msk = [None]*P['dim'][0][2]
+
+            for x3 in range(0,P['dim'][0][2]):
+                tmp = np.zeros((P['dim'][0][0],P['dim'][0][1]))
+                for i in range(0,P.size):
+                    tmp += self.getmask(np.linalg.inv(P['mat'][0]/P['mat'][i]),x1,x2,x3, ...,
+                                   P['dim'][i][0:3],flags.wrap)
+
+            if flags.mask:
+                msk[x3] = np.argwhere(tmp != P.size)
+
+            if flags.mean:
+                Count[:,:,x3] = tmp
+
+        nread = P.size
+        if not flags.mean:
+            if flags.which == 1:
+                nread = nread - 1
+
+            if flags.which == 0:
+                nread = nread = 0
+
+        x1, x2 = np.mgrid[1:P['dim'][0][0],1:P['dim'][0][1]]
+        nread = 0
+        tempD = np.array([1, 1, 1], ndmin=2)*flags.interp
+        d = np.hstack((tempD.T, flags.wrap.T))
+
+        for i in range(1,2): # range(0,P.size)
+
+            if (i>1 and flags.which==1) or flags.which==2:
+                write_vol = 1
+            else:
+                write_vol=0
+            if write_vol or flags.mean:
+                read_vol = 1
+            else:
+                read_vol = 0
+
+            if read_vol:
+                if not np.isfinite(flags.interp):
+                    v = np.abs(self.kspace3d(spm_bsplinc(P[i], np.zeros((3,2))), ...,
+                                             P['mat'][0]/P['mat'][i]))
+                for x3 in range(0,P['dim'][0][2]):
+                    if flags.mean:
+                        Integral[:,:,x3] += self.nan2zero(v[:,:,x3] * self.getmask(np.linalg.inv(P['mat'][0]/P['mat'][i]),x1,x2,x3, ...,
+                                   P['dim'][i][0:3],flags.wrap))
+                    if flags.mask:
+                        tmp = v[:,:,x3]
+                        tmp[msk[x3]] = 0
+                        v[:,:,x3] = tmp
+                else:
+                    # was commented in matlab version
+                    # C = spm_dsplinc(P[i], d)
+                    v = np.zeros(P['dim'][0])
+                    for x3 in range(0,P['dim'][0][2]):
+                        tmp, y1, y2, y3 = self.getmask(np.linalg.inv(P['mat'][0]/P['mat'][i]),x1,x2,x3, ...,
+                                   P['dim'][i][0:3],flags.wrap)
+                        v[:,:,x3] = spm_bsplins(P['C'][i], y1, y2, y3, d)
+                        # TODO: equivalent for python; was commented in matalb version
+                        # v(~tmp)      = 0
+
+                        if flags.mean:
+                            Integral[:, :, x3] += self.nan2zero(v[:,:,:x3])
+
+                        if flags.mask:
+                            tmp = v[:, :, x3]
+                            tmp[msk[x3]] = 0
+                            v[:, :, x3] = tmp
+
+                if write_vol:
+                    V0 = v
+
+                nread = nread + 1
+
+
+    def kspace3d(self, v, M):
+
+        S0, S1, S2, S3 = self.shear_decomp(M)
+
+        d = np.append(np.asarray(v.shape),[1, 1, 1]).astype(int)
+        g = (2 ** np.ceil(np.log2(d))).astype(int)
+        if (g != d).any():
+            tmp = v
+            v = np.zeros(g)
+            v[1:d[0], 1:[1], 1:d[3]] = tmp
+
+        # XY-shear
+        tmp1_1 = np.append(np.array(range(0,int((g[2]-1)/2)+1)), 0)
+        tmp1_2 = np.append(tmp1_1, np.array(range(int((-g[2]/2))+1,0)))
+        tmp1 = -1j * 2*np.pi * tmp1_2 / g[2]
+        for j in range(1,g[1]+1):
+            tempT = np.exp(np.transpose(j*S3[2][1] + S3[2][0]*np.array(range(1,g[0]+1)) + S3[2,3])*tmp1)
+            t = tempT.reshape(g[0], 1, g[2])
+            v[:,j-1,:] = np.real(np.fft.ifftn(np.fft.fftn(v[:,j-1,:], axis=2)*t,axis=2))
+
+        # XZ-shear
+        tmp1_1 = np.append(np.array(range(0, int((g[1] - 1) / 2) + 1)), 0)
+        tmp1_2 = np.append(tmp1_1, np.array(range(int((-g[1] / 2)) + 1, 0)))
+        tmp1 = -1j * 2 * np.pi * tmp1_2 / g[1]
+        for k in range(1, g[2]+1):
+            t = np.exp(np.transpose(k * S2[2][1] + S2[2][0] * np.array(range(1, g[0] + 1)) + S2[2, 3]) * tmp1)
+            v[:, :, k-1] = np.real(np.fft.ifftn(np.fft.fftn(v[:, :, k-1], axis=1) * t, axis=1))
+
+        # YZ-shear
+        tmp1_1 = np.append(np.array(range(0, int((g[2] - 1) / 2) + 1)), 0)
+        tmp1_2 = np.append(tmp1_1, np.array(range(int((-g[2] / 2)) + 1, 0)))
+        tmp1 = -1j * 2 * np.pi * tmp1_2 / g[2]
+        for k in range(1, g[2]+1):
+            t = np.exp(tmp1.T*(k * S1[0][2] + S1[0][1] * np.array(range(1, g[1] + 1)) + S1[0, 3]))
+            v[:, :, k-1] = np.real(np.fft.ifftn(np.fft.fftn(v[:, :, k-1], axis=0) * t, axis=0))
+
+        # XY-shear
+        tmp1_1 = np.append(np.array(range(0,int((g[2]-1)/2)+1)), 0)
+        tmp1_2 = np.append(tmp1_1, np.array(range(int((-g[2]/2))+1,0)))
+        tmp1 = -1j * 2*np.pi * tmp1_2 / g[2]
+        for j in range(1,g[1]+1):
+            tempT = np.exp(np.transpose(j*S0[2][1] + S0[2][0]*np.array(range(1,g[0]+1)) + S0[2,3])*tmp1)
+            t = tempT.reshape(g[0], 1, g[2])
+            v[:,j-1,:] = np.real(np.fft.ifftn(np.fft.fftn(v[:,j-1,:], axis=2)*t,axis=2))
+
+        if (g != d).any():
+            v = v[1:d[0], 1:d[1], 1:d[2]]
+
+        return v
+
+    def shear_decomp(self, A):
+
+        A0 = A[0:3][0:3]
+        if (np.abs(np.linalg.svd(A0)-1) > 1e-7).any():
+            # TODO: error message class needed
+            # error('Can''t decompose matrix')
+            o=1
+
+        t = A0[1][2]
+        if t==0:
+            t=sys.float_info.epsilon
+
+        tmp = np.array([[A0[0][1], A0[0][2]],[A0[1][1], A0[1][2]]]).T
+        a0 = np.linalg.pinv(tmp)@np.array([(A0[2][1]-(A0[1][1]-1)/t), (A0[2][2]-1)], ndmin=2).T
+        S0 = np.array([[1, 0, 0], [0, 1, 0], [a0[0], a0[1], 1]])
+        A1 = S0 / A0
+
+        tmp = np.array([[A0[1][1], A0[1][2]],[A0[2][1], A0[2][2]]]).T
+        a1 = tmp@np.array([A1[0][1], A1[0][2]], ndmin=2).T
+        S1 = [[1, a1[0], a1[1]],[0, 1, 0],[0, 0, 1]]
+        A2 = S1 / A1
+
+        tmp = np.array([[A0[0][0], A0[0][2]],[A0[0][2], A0[2][2]]]).T
+        a2 = tmp@np.array([A2[1][0], A2[1][2]], ndmin=2).T
+        S2 = [[1, 0, 0], [a2[0], 1, a2[1]], [0, 0, 1]]
+        A3 = S2 / A2
+
+        tmp = np.array([[A0[0][0], A0[0][1]],[A0[0][1], A0[1][1]]]).T
+        a3 = tmp@np.array([A3[2][0], A2[2][1]], ndmin=2).T
+        S3 = [[1, 0, 0],[0, 1, 0],[a3[0], a3[1], 1]]
+
+        s3 = A[2][3] - a0[0]*A[0][3] - a0[1]*A[1][3]
+        s1 = A[0][3] - a1[0]*A[1][3]
+        s2 = A[1][3]
+        S0 = np.vstack((np.hstack((S0, np.array([0, 0, s3], ndmin=2).T)),np.array([0, 0, 0, 1], ndmin=2)))
+        S1 = np.vstack((np.hstack((S1, np.array([s1, 0, 0], ndmin=2).T)),np.array([0, 0, 0, 1], ndmin=2)))
+        S2 = np.vstack((np.hstack((S2, np.array([0, s2, 0], ndmin=2).T)),np.array([0, 0, 0, 1], ndmin=2)))
+        S3 = np.vstack((np.hstack((S3, np.array([0, 0, 0], ndmin=2).T)),np.array([0, 0, 0, 1], ndmin=2)))
+
+        return {'S0' : S0, 'S1' : S1, 'S2': S2, 'S3': S3}
+
+    def getmask(self, M, x1, x2, x3, dim, wrp):
+
+        tiny = 5e-2 # From spm_vol_utils.c
+        y1 = M[0][0]*x1 + M[0][1]*x2 + (M[0][2]*x3 + M[0][3])
+        y2 = M[1][0]*x1 + M[1][1]*x2 + (M[1][2]*x3 + M[1][3])
+        y3 = M[2][0]*x1 + M[2][1]*x2 + (M[2][2]*x3 + M[2][3])
+        Mask = np.array([True]*y1.size).reshape(y1.shape)
+        if not wrp[0]:
+            Mask = Mask and (y1 >= (1-tiny) and y1 <= (dim[0]+tiny))
+        if not wrp[1]:
+            Mask = Mask and (y1 >= (1-tiny) and y1 <= (dim[1]+tiny))
+        if not wrp[2]:
+            Mask = Mask and (y1 >= (1-tiny) and y1 <= (dim[2]+tiny))
+
+        return { 'Mask' : Mask, 'y1' : y1, 'y2' : y2, 'y3' : y3 }
+
+    def nan2zero(self, vi):
+        return np.nan_to_num(vi, copy=True, nan=0, posinf=0, neginf=0)
---
+++ tests/read_reslice_realign/re_ad_and_align_and_slice.m
@@ -0,0 +1,53 @@
+load('mainLoopData.mat')
+load('P.mat')
+
+inpFileName = "001_000007_000006.dcm";
+
+A0=[];x1=[];x2=[];x3=[];wt=[];deg=[];b=[];
+R(1,1).mat = mainLoopData.matTemplMotCorr;
+R(1,1).dim = mainLoopData.dimTemplMotCorr;
+R(1,1).Vol = mainLoopData.imgVolTempl;
+
+matVol = mainLoopData.matVol;
+matTemplMotCorr = mainLoopData.matTemplMotCorr;
+dicomInfoVox = mainLoopData.dicomInfoVox;
+dimTemplMotCorr = mainLoopData.dimTemplMotCorr;
+dimVol = mainLoopData.dimVol;
+slNrImg2DdimX = mainLoopData.slNrImg2DdimX;
+slNrImg2DdimY = mainLoopData.slNrImg2DdimY;
+img2DdimX = mainLoopData.img2DdimX;
+img2DdimY = mainLoopData.img2DdimY;
+
+indVol = 6;
+
+dcmData = double(dicomread(inpFileName));
+R(2,1).mat = matVol;
+tmpVol = img2Dvol3D(dcmData, slNrImg2DdimX, slNrImg2DdimY, dimVol);
+
+if P.isZeroPadding
+    zeroPadVol = zeros(dimVol(1),dimVol(2),P.nrZeroPadVol);
+    dimVol(3) = dimVol(3)+P.nrZeroPadVol*2;
+    R(2,1).Vol = cat(3, cat(3, zeroPadVol, tmpVol), zeroPadVol);
+else
+    R(2,1).Vol = tmpVol;
+end
+R(2,1).dim = dimVol;
+
+flagsSpmRealign = struct('quality',.9,'fwhm',5,'sep',4,...
+    'interp',4,'wrap',[0 0 0],'rtm',0,'PW','','lkp',1:6);
+flagsSpmReslice = struct('quality',.9,'fwhm',5,'sep',4,...
+    'interp',4,'wrap',[0 0 0],'mask',1,'mean',0,'which', 2);
+
+%% realign
+[R, A0, x1, x2, x3, wt, deg, b, nrIter] = ...
+    spm_realign_rt(R, flagsSpmRealign, indVol,  ...
+    P.nrSkipVol + 1, A0, x1, x2, x3, wt, deg, b);
+
+%% reslice
+if P.isZeroPadding
+    tmp_reslVol = spm_reslice_rt(R, flagsSpmReslice);
+    reslVol = tmp_reslVol(:,:,P.nrZeroPadVol+1:end-P.nrZeroPadVol);
+    dimVol(3) = dimVol(3) - P.nrZeroPadVol*2;
+else
+    reslVol = spm_reslice_rt(R, flagsSpmReslice);
+end
