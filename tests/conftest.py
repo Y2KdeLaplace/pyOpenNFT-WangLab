618f2f0a8edeb2f87890bd79f2ff3d22f7aa11d8
Eugene Prilepin <esp.home@gmail.com>
2021-01-26 12:07:57 +0000

    add tests stub for pyspm

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,36 @@
 # -*- coding: utf-8 -*-
+
+from pathlib import Path
+
+import pytest
+import pydicom
+import numpy as np
+from scipy.io import loadmat
+
+
+@pytest.fixture(scope='session')
+def root_path() -> Path:
+    return Path(__file__).parent
+
+
+@pytest.fixture(scope='session')
+def data_path(root_path) -> Path:
+    return root_path / 'data'
+
+
+@pytest.fixture(scope='session')
+def dcm_image(data_path: Path) -> np.array:
+    fp = data_path / '001_000007_000006.dcm'
+    return pydicom.dcmread(fp).pixel_array
+
+
+@pytest.fixture(scope='session')
+def main_loop_data(data_path: Path) -> dict:
+    fp = str(data_path / 'mainLoopData.mat')
+    return loadmat(fp)
+
+
+@pytest.fixture(scope='session')
+def p_struct(data_path: Path) -> dict:
+    fp = str(data_path / 'P.mat')
+    return loadmat(fp)
---
+++ tests/pyspm/test_reslice.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+
+import pyspm
+
+
+def test_reslice_rt(dcm_image: np.array, main_loop_data: dict, p_struct: dict):
+    assert False, "Not implemented"
