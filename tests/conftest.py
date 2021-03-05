9bb95aaef20d0cf4b254b3448af8ed098954f2ac
TheAmid <45180999+TheAmid@users.noreply.github.com>
2021-03-05 18:09:10 +0400

    additional test files

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,26 +8,36 @@ import nibabel as nib
 import numpy as np
 from scipy.io import loadmat
 
-
+# main paths
 @pytest.fixture(scope='session')
 def root_path() -> Path:
     return Path(__file__).parent
-
 
 @pytest.fixture(scope='session')
 def data_path(root_path) -> Path:
     return root_path / 'data'
 
 @pytest.fixture(scope='session')
+def second_data_path(data_path: Path) -> Path:
+    return data_path / 'second test'
+
+@pytest.fixture(scope='session')
+def third_data_path(data_path: Path) -> Path:
+    return data_path / 'third test'
+
+# epi template
+@pytest.fixture(scope='session')
 def nii_image_1(data_path: Path) -> nib.nifti1.Nifti1Image:
     fp = data_path / 'fanon-0007-00006-000006-01.nii'
     return nib.load(fp, mmap=False)
 
+# first test dcm
 @pytest.fixture(scope='session')
 def dcm_image(data_path: Path) -> np.array:
     fp = data_path / '001_000007_000006.dcm'
     return pydicom.dcmread(fp).pixel_array
 
+# matlab resuls and settings
 @pytest.fixture(scope='session')
 def main_loop_data(data_path: Path) -> dict:
     fp = str(data_path / 'mainLoopData.mat')
