6e712a78eddc715b67fc7a660aaecf6a84ed49d8
da0c <artniko@gmail.com>
2022-03-16 09:36:40 +0400

    more makeup

--- opennft/nftsession.py
+++ opennft/nftsession.py
@@ -71,41 +71,6 @@ class NftIteration():
         self.deg = []
         self.b = []
 
-        # вот такая у меня мысль с набором хендлеров
-        # возможные косяки -
-        # 0. возможно не стоит так заморачиваться и подвесить плагины на RTTI
-        #    сделать такой массив хендлеров только для плагинов, а когда их вызывать
-        #    определять по декоратору - типа https://github.com/gahjelle/pyplugs
-        # 1. сделать insert сложно, но возможно все плагины надо вставлять на этапе создания
-        # 2. весь функционал хендлеров стоит сделать в базовом классе, и от него наследовать Сессию и Итерацию
-        self.handlers = {}
-        self.handlers['load_scan'] = {}
-        self.handlers['process_scan'] = {}
-        self.handlers['compute_signals'] = {}
-        self.handlers['compute_feedback'] = {}
-
-        self.handlers_data = {}
-        self.handlers_data['load_scan'] = {}
-        self.handlers_data['process_scan'] = {}
-        self.handlers_data['compute_signals'] = {}
-        self.handlers_data['compute_feedback'] = {}
-
-        self.set_basic_handlers()
-
-    # --------------------------------------------------------------------------
-    def set_basic_handlers(self):
-        self.handlers['load_scan']['load_mr_vol'] = MrVol.load_vol
-
-        self.handlers_data['load_scan']['load_mr_vol'] = None
-
-    # --------------------------------------------------------------------------
-    def dispatch_handlers(self):
-        self.iter_number += 1
-
-        # понятное дело что надо сделать цикл по всем хендлерам
-        self.handlers['load_scan']['load_mr_vol'](*self.handlers_data['load_scan']['load_mr_vol'])
-
-    # или без выпендрежа с хендлерами
     # --------------------------------------------------------------------------
     def load_vol(self, file_name, im_type):
         # возможно стоит сделать по задел под мультимодальность
@@ -121,9 +86,11 @@ class NftIteration():
         self.mr_vol.reslice(r)
         self.mr_vol.smooth()
 
+    # --------------------------------------------------------------------------
     def process_time_series(self):
         self.mr_time_series.acquiring(self.session.config.type, self.mr_vol, self.session.rois)
 
+    # --------------------------------------------------------------------------
     # test function
     def save_time_series(self):
 
